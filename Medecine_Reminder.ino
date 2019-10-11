da41d3ea903044c93725b7a75adca6b1e4000f38
Nivelle Mendiola <mendiolanivelle@gmail.com>
2019-10-12 03:01:12 +0800

    Alarm is now working

--- Medecine_Reminder.ino
+++ Medecine_Reminder.ino
@@ -1,22 +1,34 @@
 
 #include <DS3231.h>
 DS3231  rtc(A4, A5);
-char alarm = "06:12";
+String AlarmMorning = "02:56";
+String AlarmAfternoon = "12:00";
+String AlarmEvening = "06:00";
 
 
 void setup() {
   rtc.begin();
   Serial.begin(9600);
-
 }
 
 void loop() {
   Serial.println(rtc.getTimeStr(FORMAT_SHORT));
-  delay(1000);
-  if(rtc.getTimeStr(FORMAT_SHORT) == "06 : 13")
+  String CurrentTime = rtc.getTimeStr(FORMAT_SHORT);
+  delay(10000);
+  if(CurrentTime == AlarmMorning)
   {
     Serial.println("Time to Drink your Antibiotic Medicine");
   }
 
 
+}
+
+void setdate(int date, int month, int year)
+{
+  rtc.setDate(date,month,year);
+}
+
+void setTime(int hour,int minutes)
+{
+  rtc.setTime(hour,minutes,00);
 }
